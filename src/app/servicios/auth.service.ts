f0520536a6e5b629efd98201a5443a241dc22e24
Diego <dimamuno@espol.edu.ec>
2019-08-23 20:59:25 -0500

    Se ha completado el servicio de autentificacion

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,18 +1,80 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
+import { isNullOrUndefined } from 'util';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
 
-    constructor() { }
+    constructor(private http: HttpClient) { }
 
-    setToken() {
+    headers: HttpHeaders = new HttpHeaders({
+        "Content-Type": "aplication/json"
+    });
 
+    registrarUsuario(matricula: string, nombre: string, apellido: string, telefono: string, direccion: string, correo: string, contrasenia: string) {
+        const url_api = ""
+        return this.http.post(url_api, {
+            matricula: matricula,
+            nombre: nombre,
+            apellido: apellido,
+            telefono: telefono,
+            direccion: direccion,
+            correo: correo,
+            contrasenia:
+            contrasenia
+        },
+        {headers:this.headers}
+        )
+        .pipe(map(data=>data));
     }
 
-    getToken(): string{
-        let token = "";
-        return token;
+    ingresoUsuario(correo:string,contrasenia:string): Observable<any>{
+        const url_api = ""
+        return this.http.post(url_api,{
+            correo:correo,
+            contrasenia:contrasenia
+        },
+        {
+            headers:this.headers
+        }
+        )
+        .pipe(map(data=>data));
+    }
+
+    setUser(usuario):void {
+        let usuario_string = JSON.stringify(usuario);
+        localStorage.setItem("usuarioActual",usuario_string);
+    }
+
+    setToken(token):void{
+        localStorage.setItem("tokenAcceso",token);
+    }
+
+    getToken(): string {
+        return localStorage.getItem("tokenAcceso");
+    }
+
+    getCurrentUser(){
+        let usuario_string = localStorage.getItem("usuarioActual");
+        if(!isNullOrUndefined(usuario_string)){
+            let usuario = JSON.parse(usuario_string);
+            return usuario;
+        }else{
+            return null;
+        }
+    }
+
+    logoutUser(){
+        let tokenAcceso = localStorage.getItem("tokenAcceso");
+        const url_api = "";
+        localStorage.removeItem("tokenAcceso");
+        localStorage.removeItem("usuarioActual");
+        return this.http.post(url_api,
+            {headers: this.headers}
+            )
     }
 }
