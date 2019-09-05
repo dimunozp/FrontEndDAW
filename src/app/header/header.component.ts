3a4d5e2141550227c3041d9574c5f214f9d90d86
Diego <dimamuno@espol.edu.ec>
2019-09-05 00:36:49 -0500

    Loggin Implementado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import * as introJs from 'intro.js/intro.js';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+var bandera = 0;
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor(private httpClient: HttpClient, private route: Router, public location: Location) { }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
+    }
 
-  iniciarRecorrido(){
-    introJs().start();
-  }
+    iniciarRecorrido() {
+        introJs().start();
+    }
+
+    ngAfterViewInit() {
+        let boton = document.getElementById('ingreso');
+        let botons = document.getElementById('salida');
+        if (localStorage.length != 0) {
+            this.httpClient.get(`http://192.168.0.24:3000/api/usuarios/${localStorage.getItem('accessToken')}`)
+                .subscribe(
+                    (data: any) => {
+                        if (data.length == 0) {
+                            console.log("No exite el usuario");
+                        } else {
+                            let nombre = data[0].nombreUsuario;
+                            boton.innerText = nombre;
+                            botons.style.setProperty('display', 'inline');
+                            this.route.navigateByUrl('/usuario/perfil', { skipLocationChange: true })
+                                .then(() => { this.route.navigate([decodeURI(this.location.path())]) });
+                        }
+                    })
+
+
+        } else {
+            boton.innerText = "Ingresar";
+        }
+    }
+
+    logOut() {
+        localStorage.removeItem('accessToken');
+        let boton = document.getElementById('ingreso');
+        let botons = document.getElementById('salida');
+        boton.innerText = "Ingresar";
+        botons.style.setProperty('display', 'none');
+
+    }
 
 }
--- src/app/new-login/new-login.component.ts
+++ src/app/new-login/new-login.component.ts
@@ -2,58 +2,49 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { helpers } from 'chart.js';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-new-login',
-  templateUrl: './new-login.component.html',
-  styleUrls: ['./new-login.component.css']
+    selector: 'app-new-login',
+    templateUrl: './new-login.component.html',
+    styleUrls: ['./new-login.component.css']
 })
 export class NewLoginComponent implements OnInit {
 
-  constructor(private httpClient:HttpClient) { }
+    constructor(private httpClient: HttpClient,private router:Router,public location:Location) { }
 
 
-  onSubmit(loginForm:NgForm):void{
-    var user = loginForm.value;
-    console.log(user.email);
-    console.log(user.password);
-    /*
-    this.httpClient.post(`http://192.168.0.24:3000/login`,{
-      email:user.email,
-      password:user.password
-    
-  },{
-    headers: new HttpHeaders({
-      "Access-Control-Allow-Methods":"GET, POST",
-      "Access-Control-Allow-Origin":"*"
-  })
-})
-    .subscribe(
-      (data:any[]) => {
-        console.log(data);
-    })*/
+    onSubmit(loginForm: NgForm): void {
+        var user = loginForm.value;
+        console.log(user.email);
+        console.log(user.password);
 
-    fetch("http://192.168.0.24:3000/login", {
-            method: 'POST',
-            body: JSON.stringify({
-              email:user.email,
-              password:user.password
-            
-          }),
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                "Accept": 'application/json',
-            }
+        this.httpClient.post(`http://192.168.0.24:3000/api/verificarUsuario`, {
+            correo: user.email,
+            password: user.password
+
         })
-    .then((data) => data.json())
-    .then((resp) => console.log(resp))
-    .catch((err) => console.log(err))
-  }
+        .subscribe(
+            (data: any) => {
+                if(data.msg.length==0){
+                    console.log("Credenciales Incorrectas");
+                }else{
+                    console.log(data.msg[0].cedula)
+                    localStorage.setItem('accessToken',data.msg[0].cedula);
+                    window.location.reload();
+                }
+            }
+        );
 
-  ngOnInit() {
-  }
 
+    }
+
+    ngOnInit() {
+    }
+    async delay(ms: number) {
+        await new Promise(resolve => setTimeout(()=>resolve(), ms)).then(()=>console.log("fired"));
+    }
 
 
 }
