e29ef86260ea8b49928a4c37b254bfccb6ddb6ad
Diego <dimamuno@espol.edu.ec>
2019-09-04 19:52:01 -0500

    Evento en mapa colocado

--- src/app/buscar-objeto-perdido/buscar-objeto-perdido.component.ts
+++ src/app/buscar-objeto-perdido/buscar-objeto-perdido.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 declare let L;
+let facultad = "fiec" ;
 
 import { HttpClient } from '@angular/common/http';
+import { Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-buscar-objeto-perdido',
@@ -22,8 +24,11 @@ export class BuscarObjetoPerdidoComponent implements OnInit {
             fillColor: '#f03',
             fillOpacity: 0.2,
             radius: 20
-        }).addTo(map);;
+        }).addTo(map);
         circle.bindPopup("FIEC").openPopup();
+        circle.onclick=function cambiarNombre(){
+            facultad = "fiec";
+        }
 
         var circle2 = L.circle([-2.148636, -79.967612], {
             color: 'yellow',
@@ -32,6 +37,9 @@ export class BuscarObjetoPerdidoComponent implements OnInit {
             radius: 20
         }).addTo(map);
         circle2.bindPopup("CELEX").openPopup();
+        circle2.onclick=function cambiarNombre(){
+            facultad = "celex";
+        }
 
         var circle3 = L.circle([-2.147180, -79.967793], {
             color: 'brown',
@@ -40,6 +48,9 @@ export class BuscarObjetoPerdidoComponent implements OnInit {
             radius: 20
         }).addTo(map);
         circle3.bindPopup("FCSH").openPopup();
+        circle3.onclick=function cambiarNombre(){
+            facultad = "fcsh";
+        }
 
         var circle4 = L.circle([-2.152011, -79.955884], {
             color: 'green',
@@ -48,6 +59,9 @@ export class BuscarObjetoPerdidoComponent implements OnInit {
             radius: 20
         }).addTo(map);
         circle4.bindPopup("FCV").openPopup();
+        circle4.onclick=function cambiarNombre(){
+            facultad = "fcv";
+        }
 
         var circle5 = L.circle([-2.146687, -79.963236], {
             color: 'blue',
@@ -56,6 +70,9 @@ export class BuscarObjetoPerdidoComponent implements OnInit {
             radius: 20
         }).addTo(map);
         circle5.bindPopup("FIMCBOR").openPopup();
+        circle5.onclick=function cambiarNombre(){
+            facultad = "fimcbor";
+        }
 
         var circle6 = L.circle([-2.146378, -79.967025], {
             color: 'purple',
@@ -64,6 +81,9 @@ export class BuscarObjetoPerdidoComponent implements OnInit {
             radius: 20
         }).addTo(map);
         circle6.bindPopup("FCNM").openPopup();
+        circle6.onclick=function cambiarNombre(){
+            facultad = "fcnm";
+        }
 
         var circle7 = L.circle([-2.145540, -79.965359], {
             color: 'black',
@@ -72,6 +92,9 @@ export class BuscarObjetoPerdidoComponent implements OnInit {
             radius: 20
         }).addTo(map);
         circle7.bindPopup("FICT").openPopup();
+        circle7.onclick=function cambiarNombre(){
+            facultad = "fict";
+        }
 
         var circle8 = L.circle([-2.145024, -79.965760], {
             color: 'silver',
@@ -80,6 +103,10 @@ export class BuscarObjetoPerdidoComponent implements OnInit {
             radius: 20
         }).addTo(map);
         circle8.bindPopup("FIMCP").openPopup();
+        circle8.onclick=function cambiarNombre(){
+            facultad = "fimcp";
+            console.log(facultad);
+        }
 
         var circle9 = L.circle([-2.143581, -79.962095], {
             color: 'pink',
@@ -88,8 +115,65 @@ export class BuscarObjetoPerdidoComponent implements OnInit {
             radius: 20
         }).addTo(map);
         circle9.bindPopup("FADCOM").openPopup();
+        circle9.onclick=function cambiarNombre(){
+            console.log(facultad);
+            facultad = "fadcom";
+            console.log(facultad);
+        }
     }
 
+    ngAfterViewInit() {
+        let circulos = document.getElementsByClassName("leaflet-interactive");
+        for(let i=0;i<circulos.length;i++){
+            let circulo = circulos[i];
+            if(circulo.getAttribute('fill')=='blue'){
+                circulo.addEventListener('click',this.onClick.bind(this));
+            }else if(circulo.getAttribute('fill')=='#f03'){
+                circulo.addEventListener('click',this.onClick2.bind(this));
+            }else if(circulo.getAttribute('fill')=='yellow'){
+                circulo.addEventListener('click',this.onClick3.bind(this));
+            }else if(circulo.getAttribute('fill')=='brown'){
+                circulo.addEventListener('click',this.onClick4.bind(this));
+            }else if(circulo.getAttribute('fill')=='green'){
+                circulo.addEventListener('click',this.onClick5.bind(this));
+            }else if(circulo.getAttribute('fill')=='purple'){
+                circulo.addEventListener('click',this.onClick6.bind(this));
+            }else if(circulo.getAttribute('fill')=='black'){
+                circulo.addEventListener('click',this.onClick7.bind(this));
+            }else if(circulo.getAttribute('fill')=='silver'){
+                circulo.addEventListener('click',this.onClick8.bind(this));
+            }else if(circulo.getAttribute('fill')=='pink'){
+                circulo.addEventListener('click',this.onClick9.bind(this));
+            }
+        }
+    }
+    onClick(event){
+        facultad = "fimcbor";
+    }
+    onClick2(event){
+        facultad = "fiec";
+    }
 
+    onClick3(){
+        facultad = "celex";
+    }
 
+    onClick4(){
+        facultad = "fcsh";
+    }
+    onClick5(){
+        facultad = "fcv";
+    }
+    onClick6(){
+        facultad = "fcnm";
+    }
+    onClick7(){
+        facultad = "fict";
+    }
+    onClick8(){
+        facultad = "fimcp";
+    }
+    onClick9(){
+        facultad = "fadcom";
+    }
 }
